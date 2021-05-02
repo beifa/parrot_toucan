200427593261813e477b2d8b1c132b8297af5bc9
beifa <beifabeifa@mail.ru>
2021-05-02 14:46:06 +0300

    add seed method

--- project2_rcnn/src/utils.py
+++ project2_rcnn/src/utils.py
@@ -75,3 +75,11 @@ def plot_rectangle(out: list, image: list, threshold: int, color: tuple, outline
             return image
         return image
     return image
+
+def set_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True

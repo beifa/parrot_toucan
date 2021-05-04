1645adda68581447d14b8bb73e4306fcd388bc17
beifa <beifabeifa@mail.ru>
2021-05-04 14:43:33 +0300

    change to argumentation

--- project2_rcnn/src/dataset.py
+++ project2_rcnn/src/dataset.py
@@ -11,26 +11,40 @@ class PT(Dataset):
 
     def __len__(self):
         return len(self.data)
+     
 
     def __getitem__(self, idx):
      
         dt = self.data[idx]        
         img = dt['image']        
         box_axis = dt['objects'][0]['points']['exterior']
-        area = (box_axis[1][1] - box_axis[0][1]) * (box_axis[1][0] - box_axis[0][0])        
-        boxes = torch.as_tensor(box_axis, dtype=torch.float32)
-        boxes = boxes.reshape(-1, 4)
+        area = (box_axis[1][1] - box_axis[0][1]) * (box_axis[1][0] - box_axis[0][0])  
         
-        target = {}
-        target['boxes'] = boxes
+        target = {}      
         target['labels'] = torch.ones((1), dtype = torch.long)
         target['image_id'] = torch.tensor([idx], dtype = torch.float32)
         target['area'] = torch.tensor(area, dtype = torch.float32)
+        
+        # add aurgum
+        if self.transforms:
+            sample = {
+                'image': img,
+                'bboxes':[np.array(box_axis).ravel()],
+                'labels': ['1']
+                }
+            
+            sample = self.transforms(**sample)
+            img = sample['image']
+       
         img = img / 255.0
-        img = img.transpose(2, 0, 1).astype(np.float32)   
-        if self.transforms is not None:
-            img = self.transforms(img)   
-        return torch.tensor(img, dtype = torch.float32), target 
+        img = img.transpose(2, 0, 1).astype(np.float32)  
+
+        bboxes = torch.as_tensor(sample['bboxes'], dtype=torch.float32)
+        bboxes = bboxes.reshape(-1, 4)
+        target['boxes'] = bboxes
+
+        # torch.tensor(img, dtype = torch.float32) 
+        return torch.tensor(img, dtype = torch.float32), target  
         
 
 class PT_test(Dataset):
